636b66d39e1319dd7dc4b43d5515d3e619ce64cb
kurozumi <info@a-zumi.net>
2021-06-26 11:20:22 +0900

    テスト追加

---
+++ Tests/Web/EntryControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of HTMLPurifier
+ *
+ * Copyright(c) Akira Kurozumi <info@a-zumi.net>
+ *
+ *  https://a-zumi.net
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\HTMLPurifier\Tests\Web;
+
+
+class EntryControllerTest extends \Eccube\Tests\Web\EntryControllerTest
+{
+    public function test住所に攻撃性のある文字列を入力してサブミットしたらその文字列は削除される未入力エラーが発生するか()
+    {
+        $formData = $this->createFormData();
+        $formData['address']['addr01'] = '<script>';
+
+        $crawler = $this->client->request('POST',
+            $this->generateUrl('entry'),
+            [
+                'entry' => $formData,
+                'mode' => 'confirm',
+            ]
+        );
+
+        self::assertEquals('新規会員登録', $crawler->filter('.ec-pageHeader > h1')->text());
+        self::assertCount(1, $crawler->filter('.ec-errorMessage'));
+        self::assertTrue($this->client->getResponse()->isSuccessful());
+    }
+}
